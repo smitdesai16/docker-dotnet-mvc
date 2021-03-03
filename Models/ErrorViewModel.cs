9545168615edfd7c214fe516f35f3a0c1a994b8a
Smit Desai <smit.desai16@gmail.com>
2021-03-03 15:30:18 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace docker_dotnet_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
